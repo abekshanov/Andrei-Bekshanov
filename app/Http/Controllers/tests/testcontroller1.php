53b916a3de871c29cefbeb446ec665853ce0adba
abekshanov <b_and_work@mail.ru>
2019-11-26 13:52:30 +0300

    added links from https://getbootstrap.com/ for default style user inteface

--- app/Http/Controllers/tests/testcontroller1.php
+++ app/Http/Controllers/tests/testcontroller1.php
@@ -9,7 +9,6 @@ class testcontroller1 extends Controller
 {
     // для экспериментов
     public function index($title){
-        $address="pages/mvc-pages/".$title;
-       return view('pages.mvc-page', compact('title','address'));
+        return view('pages.mvc-page', compact('title'));
     }
 }
