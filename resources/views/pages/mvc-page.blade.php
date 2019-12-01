bcf762e9e2622bbd9a25e6d47c2ab0caf27898d1
abekshanov <b_and_work@mail.ru>
2019-12-01 19:07:52 +0300

    added some bootstrap classes to training-programs.blade

--- resources/views/pages/mvc-page.blade.php
+++ resources/views/pages/mvc-page.blade.php
@@ -2,5 +2,6 @@
 
 @section('content')
     <h1>{{$title}}</h1>
+
 @endsection
 
--- resources/views/pages/training-programs.blade.php
+++ resources/views/pages/training-programs.blade.php
@@ -1,8 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-       @foreach($listTrainingPrograms as $listTrainingProgram)
-           {{$listTrainingProgram->id." ".$listTrainingProgram->name}}<br>
-       @endforeach
+    <div class="container">
+        <div class="row">
+         <div class="col-4"></div>
+         <div class="col-4 btn btn-sm btn-primary  my-3 ">Добавить программу</div>
+         <div class="col-4"></div>
+        </div>
+        <div class="listgroup">
+           @foreach($listTrainingPrograms as $listTrainingProgram)
+                <a href="#!" class="list-group-item list-group-item-action list-group-item-info my-1 py-0">{{$listTrainingProgram->id." ".$listTrainingProgram->name}}</a>
+           @endforeach
+        </div> <!--endrow-->
+    </div>
 @endsection
 
